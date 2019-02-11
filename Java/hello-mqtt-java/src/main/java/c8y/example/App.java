ceea12df7a61939acd05d87318724bd96153ead4
Manasés Jesús <contact@manasesjesus.com>
2019-02-11 13:39:16 +0100

    Java initial project code

---
+++ Java/hello-mqtt-java/src/main/java/c8y/example/App.java
@@ -0,0 +1,13 @@
+package c8y.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
