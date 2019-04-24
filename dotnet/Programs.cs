fd7a43cf6e42b2238178b836aaa05729c31eb175
Manasés Jesús <contact@manasesjesus.com>
2019-04-24 14:31:42 +0200

    changing PackageReference version

--- dotnet/Programs.cs
+++ dotnet/Programs.cs
@@ -2,14 +2,15 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Cumulocity.SDK.MQTT.Model;
+using Cumulocity.SDK.MQTT.Model.ConnectionOptions;
 using Cumulocity.SDK.MQTT.Model.MqttMessage;
 using MqttClient = Cumulocity.SDK.MQTT.MqttClient;
 
-namespace MQTTClientExample
+namespace hello_mqtt_cs
 {
     class Program
     {
-        static void Main (string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Task.Run(RunClientAsync);
@@ -18,11 +19,11 @@ namespace MQTTClientExample
 
         private static async Task RunClientAsync()
         {
-            const string serverUrl = "mqtt.cumulocity.com";
+            const string serverUrl = "manga.eu-latest.cumulocity.com/mqtt";
             const string clientId = "my_mqtt_cs_client";
-            const string device_name = "My CS MQTT device";
-            const string user = "<<tenant>>/<<username>>";
-            const string password = "<<password>>";
+            const string device_name = "My C# MQTT device";
+            const string user = "manga/manga@softwareag.com";
+            const string password = "zaq12wsx.";
 
             //connections details
             var cDetails = new ConnectionDetailsBuilder()
@@ -65,9 +66,9 @@ namespace MQTTClientExample
                 .WithMessageContent("114,c8y_Restart")
                 .Build());
 
-            // generate a random temperature (10º-20º) measurement and send it every 1 s
+            // generate a random temperature (10º-20º) measurement and send it every second
             Random rnd = new Random();
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 7; i++)
             {
                 int temp = rnd.Next(10, 20);
                 Console.WriteLine("Sending temperature measurement (" + temp + "º) ...");
