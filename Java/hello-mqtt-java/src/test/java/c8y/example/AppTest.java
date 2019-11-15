777908e047597e8b373ac0dfa2a1db703ba559f8
Manasés Jesús <manasesjesus@users.noreply.github.com>
2019-11-15 16:17:38 +0100

    Update AppTest.java

--- Java/hello-mqtt-java/src/test/java/c8y/example/AppTest.java
+++ Java/hello-mqtt-java/src/test/java/c8y/example/AppTest.java
@@ -28,11 +28,4 @@ public class AppTest
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
